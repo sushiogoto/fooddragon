c2d863a7b0ba7ed697f22ede7cbd8fe110a243d4
Yoshio Goto <yoshio.goto@gmail.com>
2014-10-16 17:22:22 +0800

    Created FoodItem database and seed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
   def index
+
+    # /?admin=true
+    @current_time = Time.now + 45.minutes
+    @delivery_time = @current_time.strftime("%k:%M")
+    if params[:admin] == "true"
+      render 'index'
+    else
+      render 'under_construction', layout: false
+    end
+
   end
+
+  def construction
+    render 'under_construction'
+  end
+
 end
